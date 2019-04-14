e59ab6ee3812f4d060a1ddd3d1a2aa5b64108d27
masterUNG <phrombutr@gmail.com>
2019-04-14 14:48:55 +0700

    Change BG ok

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -6,18 +6,38 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  // Show Logo
+  Widget showLogo() {
+    return Image.asset('images/logo.png');
+  }
+
   // App Name
   Widget showAppName() {
-    return Text('Ung Restaurant');
+    return Text(
+      'Ung Restaurant',
+      style: TextStyle(
+          fontSize: 30.0, fontWeight: FontWeight.bold, color: Colors.white),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.orangeAccent[700], Colors.orangeAccent[100]],
+                begin: Alignment(-1, -1))),
         padding: EdgeInsets.only(top: 100.0),
         alignment: Alignment.topCenter,
-        child: Column(children: <Widget>[showAppName(), showAppName(), showAppName()],),
+        child: Column(
+          children: <Widget>[
+            showLogo(),
+            Container(margin: EdgeInsets.only(top: 30.0),
+              child: showAppName(),
+            )
+          ],
+        ),
       ),
     );
   }
