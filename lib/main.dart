f33a795b8afbca8680ca3be2ac3036403f14499e
masterUNG <phrombutr@gmail.com>
2019-04-14 13:26:03 +0700

    Start App

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'screens/authen.dart';
+
+// main Method นี่คือ เมธอดหลัก นะ
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Authen()
+    );
+  }
+}
