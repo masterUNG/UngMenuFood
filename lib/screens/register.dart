1b3ecb170acb0557c2f59609658f11171f99f7a3
masterUNG <phrombutr@gmail.com>
2019-04-15 11:49:33 +0700

    Design UI Register Success

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -6,12 +6,60 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  Widget uploadIcon() {
+    return IconButton(
+      icon: Icon(Icons.cloud_upload),
+      onPressed: () {
+        print('You Click Upload');
+      },
+    );
+  }
+
+  // Name
+  Widget nameTextFormField() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Name :', hintText: 'Name in Eng'),
+    );
+  }
+
+  // User
+  Widget userTextFormField() {
+    return TextFormField(
+      decoration:
+          InputDecoration(labelText: 'User :', hintText: 'fill user here'),
+    );
+  }
+
+  // Password
+  Widget passwordTextFormField() {
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: 'Password :', hintText: 'More 6 Charactor'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Register'),
-      ),body: Text('Welcome Register'),
-    );
+    return Scaffold(resizeToAvoidBottomPadding: false,
+        appBar: AppBar(
+          title: Text('Register'),
+          actions: <Widget>[uploadIcon()],
+        ),
+        body: Container(color: Colors.yellow,
+            padding: EdgeInsets.all(50.0),
+            child: Container(padding: EdgeInsets.all(20.0),
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(30.0),
+                  border: Border.all(
+                      width: 1.0,
+                      color: Colors.grey,
+                      style: BorderStyle.solid)),
+              child: Column(
+                children: <Widget>[
+                  nameTextFormField(),
+                  userTextFormField(),
+                  passwordTextFormField()
+                ],
+              ),
+            )));
   }
 }
